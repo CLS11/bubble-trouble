3abd6339ad24b544ce3af2c873e6852ea8d9be95
crystall.101600 <crystall.101600@gmail.com>
2025-02-20 06:37:03 +0000

    Integrated the missile, player, ball altogether with buttons to make the final bubble trouble.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,7 +4,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:myapp/ball.dart';
 import 'package:myapp/button.dart';
+import 'package:myapp/missile.dart';
 import 'package:myapp/player.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,14 +16,78 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+//Enum for directions
+enum directions { LEFT, RIGHT }
+
 class _HomePageState extends State<HomePage> {
   //Alignment variables
-  double playerX = 0;
+  static double playerX = 0;
+
   //Missile variables
-  double missileX = 0;
+  double missileX = playerX;
   double missileY = 0;
   double missileHeight = 0;
+  bool midShot = false;
+
+  //Converting the height to the co-ordinate
+  double heightToPosition(double height) {
+    final totalHeight = MediaQuery.of(context).size.height;
+    final position = 1 - (2 * (height / totalHeight));
+    return position;
+  }
+
+  //Ball variables
+  double ballX = 0.5;
+  double ballY = 0;
+  var ballDirection = directions.LEFT;
+
   //Methods for the movement
+  void startGame() {
+    double time = 0;
+    double height = 0;
+    double velocity = 60;
+    Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      height = -5 + time * time + velocity * time;
+      if (height < 0) {
+        time = 0;
+      }
+      setState(() {
+        ballY = heightToPosition(height);
+      });
+      if (ballX - 0.005 < -1) {
+        ballDirection = directions.RIGHT;
+      } else if (ballX + 0.005 > 1) {
+        ballDirection = directions.LEFT;
+      }
+      if (ballDirection == directions.LEFT) {
+        setState(() {
+          ballX -= 0.005;
+        });
+      } else {
+        setState(() {
+          ballX += 0.005;
+        });
+      }
+      if (playerDies()) {
+        timer.cancel();
+        _showDialog();
+      }
+      time += 0.1;
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey[700],
+          title: const Text('DEAD!', style: TextStyle(color: Colors.white)),
+        );
+      },
+    );
+  }
+
   void moveLeft() {
     setState(() {
       if (playerX - 0.1 < -1) {
@@ -29,6 +95,7 @@ class _HomePageState extends State<HomePage> {
       } else {
         playerX -= 0.1;
       }
+      missileX = playerX;
     });
   }
 
@@ -39,15 +106,47 @@ class _HomePageState extends State<HomePage> {
       } else {
         playerX += 0.1;
       }
+      if (!midShot) {
+        missileX = playerX;
+      }
     });
   }
 
   void fireMissile() {
-    Timer.periodic(Duration(milliseconds: 20), (timer) {
-      setState(() {
-        missileHeight += 10;
+    if (midShot == false) {
+      Timer.periodic(const Duration(milliseconds: 20), (timer) {
+        midShot = true;
+        setState(() {
+          missileHeight += 10;
+          missileY = heightToPosition(missileHeight);
+        });
+        if (missileHeight > MediaQuery.of(context).size.height * 3 / 4) {
+          resetMissile();
+          timer.cancel();
+        }
+        if (ballY > heightToPosition(missileHeight) &&
+            (ballX - missileX).abs() < 0.03) {
+          resetMissile();
+          ballX = 5;
+          timer.cancel();
+        }
       });
-    });
+    }
+  }
+
+  void resetMissile() {
+    missileX = playerX;
+    missileY = heightToPosition(0);
+    missileHeight = 0;
+    midShot = false;
+  }
+
+  bool playerDies() {
+    if ((ballX - playerX).abs() < 0.05 && ballY > 0.95) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   @override
@@ -74,15 +173,16 @@ class _HomePageState extends State<HomePage> {
                 child: Center(
                   child: Stack(
                     children: [
-                      Container(
-                        alignment: Alignment(missileX, missileY),
-                        child: Container(
-                          width: 5,
-                          height: missileHeight,
-                          color: Colors.grey,
-                        ),
+                      //Missile
+                      Missile(
+                        missileX: missileX,
+                        missileY: missileY,
+                        missileHeight: missileHeight,
                       ),
+                      //Player
                       Player(playerX: playerX),
+                      //Ball
+                      Ball(ballX: ballX, ballY: ballY),
                     ],
                   ),
                 ),
@@ -94,6 +194,7 @@ class _HomePageState extends State<HomePage> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
+                    Button(icon: Icons.play_arrow, function: startGame),
                     Button(icon: Icons.arrow_back, function: moveLeft),
                     Button(icon: Icons.arrow_forward, function: moveRight),
                     Button(icon: Icons.arrow_upward, function: fireMissile),
